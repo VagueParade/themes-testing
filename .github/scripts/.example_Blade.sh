8249d9b1afc7b8d062b06beb7a8fc12b2ad5b34b
VagueParade <166325657+VagueParade@users.noreply.github.com>
2024-07-04 22:00:35 +0100

    Create .example_Blade.sh

---
+++ .github/scripts/.example_Blade.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+URL="https://github.com/MustardOS/theme/releases/latest/download/Blade.zip"
+THEME_NAME="Blade"
+LOCAL_ZIP_PATH="/mnt/mmc/MUOS/theme/Blade.zip"
+LOCAL_PREVIEW="/mnt/mmc/MUOS/theme/preview/Blade.png"
+ARCHIVE_ZIP="/mnt/mmc/ARCHIVE/Blade.zip"
+SH_NAME="Blade.sh"
+CREDITS_INFO="by: LMarcoMiranda"
+
+rm -rf "$MUX_TEMP" /tmp/muxlog_*
+
+. "/mnt/mmc/MUOS/task/.ThemeInstall.sh"
